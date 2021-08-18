4493a57c2db093d5fbcc70100c288bbfd4dda439
Daniel Alvsåker <daniel@timeanddate.com>
2021-08-18 12:29:59 +0200

    Run tests with environment variables

--- TimeAndDate/tests/com/timeanddate/services/tests/Config.java
+++ TimeAndDate/tests/com/timeanddate/services/tests/Config.java
@@ -1,6 +1,6 @@
 package com.timeanddate.services.tests;
 
 public final class Config {
-	public static String AccessKey = "";
-	public static String SecretKey = "";
+	public static String AccessKey = System.getenv("ACCESS_KEY");
+	public static String SecretKey = System.getenv("SECRET_KEY");
 }
