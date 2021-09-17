9f6bba3e90ee7af2e90b3f8f884cdd6b44b8d4b4
Daniel Alvsåker <daniel@timeanddate.com>
2021-09-17 12:20:38 +0200

    Update default version to version 3

--- TimeAndDate/main/com/timeanddate/services/Constants.java
+++ TimeAndDate/main/com/timeanddate/services/Constants.java
@@ -6,7 +6,7 @@ package com.timeanddate.services;
  *
  */
 final class Constants {
-	static final int DefaultVersion = 2;
+	static final int DefaultVersion = 3;
 	static final String DefaultLanguage = "en";
 	static final String DefaultReturnFormat = "xml";
 	static final int DefaultVerboseTimeValue = 0;
